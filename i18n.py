46f9d94e5190562a318bffeb98cec58fc9b7bfd1
vtereshyn-dnipro-m <v.tereshyn@dniprom.com>
2026-08-14 11:33:22 +0300

    Update i18n.py

--- i18n.py
+++ i18n.py
@@ -1743,4 +1743,4 @@ def language_toggle(location=None):
     new_lang = order[[LANG_LABELS[l] for l in order].index(choice)]
     if new_lang != st.session_state.lang:
         st.session_state.lang = new_lang
-        st.rerun()
+        st.rerun() 
